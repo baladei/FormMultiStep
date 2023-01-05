fedf6a20db2a6462b4f7985b755d62fefcd44ff3
Fernando Baladei <f.baladei@gmail.com>
2023-01-05 06:26:33 -0300

    starting build home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import './App.css'
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Deixa sua avaliação</h2>
+        <p>Ficamos felizes com a sua compra, preencha o formulário abaixo para avaliar o produto</p>
+      </div>
+      <div className="form-conteiner">
+        <p>etapas</p>
+        <form>
+          <div className="actions">
+            <button type='button'>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type='submit'>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+
+        </form>
+      </div>
     </div>
   )
 }
